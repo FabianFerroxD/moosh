94ce85f46b2115890c54e03fe23489cf5dea6397
Fabián Ferro <64550372+FabianFerroxD@users.noreply.github.com>
2020-04-29 15:28:08 -0300

    Fix error listing

--- Moosh/Command/Moodle31/Group/GroupList.php
+++ Moosh/Command/Moodle31/Group/GroupList.php
@@ -59,7 +59,7 @@ class GroupList extends MooshCommand {
                         else {
                             echo "\tgroup " . $group->id . " \"" . $group->name . "\" " . $group->description . "\n";
                         }
-                        $dupe[$id] = $group;
+                        $dupe[$group->id] = $group;
                     }
                 }
             }
@@ -76,7 +76,7 @@ class GroupList extends MooshCommand {
                             echo "No grouping\n";
                     }
                     foreach ($free_groups as $group) {
-                        if (!empty($options["id"])) { echo $id . "\n"; }
+                        if (!empty($options["id"])) { echo $group->id . "\n"; }
                         else {
                             echo "\tgroup " . $group->id . " \"" . $group->name . "\" " . $group->description . "\n";
                         }
